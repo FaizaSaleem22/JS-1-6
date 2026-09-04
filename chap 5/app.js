608f0f86c4357529d2f6f90bbf25c0f66df7a82e
FaizaSaleem22 <faizasaleem288@gmail.com>
2026-09-04 15:25:58 +0500

    js added

--- chap 5/app.js
+++ chap 5/app.js
@@ -1,4 +1,74 @@
+// Q1
 var num1 = 3
 var num2 = 5
 var add = num1 + num2
 document.write ("Sum of "+num1+" "+ "and "+num2+" "+ "is "+add )
+document.write("<br>")
+// Q2
+var num1 = 3
+var num2 = 5
+var subtract = num1 - num2
+document.write ("Subtraction of "+num1+" "+ "and "+num2+" "+ "is "+subtract )
+document.write("<br>")
+var num1 = 3
+var num2 = 5
+var multiply = num1 * num2
+document.write ("Multiplication of "+num1+" "+ "and "+num2+" "+ "is "+multiply )
+document.write("<br>")
+var num1 = 3
+var num2 = 5
+var divide = num1 / num2
+document.write ("Division of "+num1+" "+ "and "+num2+" "+ "is "+divide )
+document.write("<br>")
+var num1 = 3
+var num2 = 5
+var remainder = num1 % num2
+document.write ("Modulus of "+num1+" "+ "and "+num2+" "+ "is "+remainder )
+document.write("<br>")
+// Q3
+var declaration = "Value after variable declaration is undefined"
+document.write (declaration)
+document.write("<br>")
+var initial = 5
+document.write ("Initial value: "+initial)
+document.write("<br>")
+++initial
+document.write ("Value after increment is: "+initial)
+document.write("<br>")
+var subsequent = initial+7
+document.write("Value after addition is: "+subsequent)
+document.write("<br>")
+subsequent--
+document.write("Value after decrement is: "+subsequent)
+document.write("<br>")
+subsequent=subsequent%3
+document.write("The reminder is: "+subsequent)
+document.write("<br>")
+// Q4
+var ticket = 600
+ticket=ticket*5
+document.write ("Total cost to buy 5 tickets is "+ticket+"PKR")
+document.write("<br>")
+// Q5
+var num=4
+document.write ("Table of "+num+"<br>")
+document.write (num+"x1="+num*1+"<br>")
+document.write (num+"x2="+num*2+"<br>")
+document.write (num+"x3="+num*3+"<br>")
+document.write (num+"x4="+num*4+"<br>")
+document.write (num+"x5="+num*5+"<br>")
+document.write (num+"x6="+num*6+"<br>")
+document.write (num+"x7="+num*7+"<br>")
+document.write (num+"x8="+num*8+"<br>")
+document.write (num+"x9="+num*9+"<br>")
+document.write (num+"x10="+num*10+"<br>")
+// Q6
+var celcius=25
+var fahrenheit= (celcius*9/5)+32
+document.write(celcius+"℃ is "+fahrenheit+"℉"+"<br>")
+var fahrenheit=70
+var celcius= (fahrenheit-32)*5/9
+document.write(fahrenheit+"℉ is "+celcius+"℃")
+
+
+
